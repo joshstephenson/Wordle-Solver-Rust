ec8002252c0241bfeabed4637eaa82099b142e94
Joshua Stephenson <josh@brightmediums.com>
2022-08-15 09:28:07 -0400

    No need to conflate word lists anymore

--- src/help.rs
+++ src/help.rs
@@ -13,19 +13,20 @@ fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
         .collect()
 }
 
-pub fn load_words(include_guesses: bool) -> Vec<String> {
-    let mut buf = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    buf.push("data/nyt-answers.txt");
-    let mut path = Path::new(&buf);
-    let answers = lines_from_file(path);
-    if include_guesses {
-        buf = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+pub fn load_words(guessables: bool) -> Vec<String> {
+    if guessables {
+        let mut buf = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
         buf.push("data/nyt-guesses.txt");
-        path = Path::new(&buf);
-        let mut guesses = lines_from_file(path);
+        let path = Path::new(&buf);
+        let guesses = lines_from_file(path);
 //        let mut answer_clone = answers.clone();
 //        guesses.append(&mut answer_clone);
-        return guesses.iter().map(|a| a.to_uppercase()).collect();
+        guesses.iter().map(|a| a.to_uppercase()).collect()
+    }else {
+        let mut buf = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        buf.push("data/nyt-answers.txt");
+        let path = Path::new(&buf);
+        let answers = lines_from_file(path);
+        answers.iter().map(|a| a.to_uppercase()).collect()
     }
-    answers.iter().map(|a| a.to_uppercase()).collect()
 }
